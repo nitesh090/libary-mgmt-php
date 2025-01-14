33b5a448f2ed4392edf517a76be40e7d6dee90ac
nitesh090 <niteshadhikari090@gmail.com>
2025-01-14 18:23:09 +0545

    book view page and navbar ui created

---
+++ inc/libloginheader.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Libary Mgmt</title>
+    <link rel="stylesheet" href="../index.css">
+</head>
+
+<body>
+    <nav class="libnav">
+        <ul>
+            <li><a href="#">Book view</a></li>
+            <li><a href="#">Book taken</a></li>
+        </ul>
+        <a href="#">Log Out</a>
+    </nav>
---
+++ lib/view.php
@@ -0,0 +1,204 @@
+<?php include_once('../inc/libloginheader.php') ?>
+<h1>Book View</h1>
+<button class="btn">Add</button>
+<div class="vbook">
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+
+    <div class="bookCard">
+        <img src="../public/img/bookDemo.jpg" alt="" class="bookCardImg">
+        <p class="bookName">book name</p>
+        <div class="bookAuthorAndDate">
+            <p class="bookAuthor">author</p>
+            <p class="bookDate">2021-20-20</p>
+        </div>
+        <p class="bookStock">Stock: <span>100</span></p>
+        <div class="bookBtnContainer">
+            <button class="bookEditBtn">edit</button>
+            <button class="bookDeleteBtn">delete</button>
+        </div>
+    </div>
+</div>
+<?php include_once('../inc/libloginfooter.php') ?>
