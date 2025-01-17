0a5145ca7c7eab71c8022f1e747ba884387486f1
nitesh090 <niteshadhikari090@gmail.com>
2025-01-17 13:33:49 +0545

    student tabel is being made

---
+++ stu/student.php
@@ -0,0 +1,22 @@
+<?php include_once('../inc/libloginheader.php') ?>
+<section class="container">
+    <h1 class="sname">Student Name</h1>
+    <div class="sbook">
+        <table class="takentabel">
+            <tr>
+                <th>Book Name</th>
+                <th>Author</th>
+                <th>Taken Date</th>
+                <th>Return Date</th>
+            </tr>
+            <tr>
+                <td>Book Name</td>
+                <td>Author</td>
+                <td>Taken Date</td>
+                <td>Return Date</td>
+
+        </table>
+    </div>
+
+</section>
+<?php include_once('../inc/libloginfooter.php') ?>
